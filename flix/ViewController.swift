20ffa548eb9d6ba391ef5909d02a3380cc859668
Farid Ramos <ramosf@purdue.edu>
2018-01-09 14:27:42 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Farid Ramos on 1/9/18.
+//  Copyright © 2018 Farid Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
